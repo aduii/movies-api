0da5a2e507f7700400fb8fa380d286c9d8b427c6
aduii <alexjuep@hotmail.com>
2021-05-14 18:06:25 -0500

    CRUD

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const app = express();
 
 const { config } = require('./config/index');
 
-app.get('/', function (req, res) {
-  res.send('Hello world');
-});
+const moviesApi = require('./routes/movies.js');
 
-app.get('/json', function (req, res) {
-  res.json({ hello: 'world' });
-});
+moviesApi(app);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
